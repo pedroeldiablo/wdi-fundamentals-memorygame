8f95ca01966c518a9a6b8d1ed5e467460ef86c30
pedroeldiablo <pedroeldiablo@yahoo.com>
2016-09-06 11:43:04 +0100

    Imported personal versions of files

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,16 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour ="king";
+
+var matchMatch = function(choiceOne, choiceTwo) {
+	if(choiceOne === "queen" && choiceTwo === "queen"){
+		alert("You found a match!");
+	}
+	else if (choiceOne === "king" && choiceTwo === "king"){
+		alert("You found a match!");
+	}	
+	else if ((choiceOne === "queen" && choiceTwo === "king") || (choiceOne === "king" && choiceTwo === "queen")){
+		alert("Sorry, try again.");
+	}
+};
